c92b1e3078241bf0c3e58aaf7864fb8985fabd86
Linghua Zeng <zenglh@outlook.com>
2019-10-28 17:59:45 +0800

    update

--- models/stackhourglass.py
+++ models/stackhourglass.py
@@ -239,9 +239,68 @@ class PSMNet(nn.Module):
         refimg_fea     = self.feature_extraction(left)
         targetimg_fea  = self.feature_extraction(right)
 
+#matching
+        cost = torch.zeros(
+            [refimg_fea.size()[0], refimg_fea.size()[1]*2, self.maxdisp//4,  refimg_fea.size()[2],  refimg_fea.size()[3]],
+            device=refimg_fea.device)
 
-        
+        for i in range(self.maxdisp//4):
+            if i > 0 :
+                cost[:, :refimg_fea.size()[1], i, :,i:]   = refimg_fea[:,:,:,i:]
+                cost[:, refimg_fea.size()[1]:, i, :,i:] = targetimg_fea[:,:,:,:-i]
+            else:
+                cost[:, :refimg_fea.size()[1], i, :,:]   = refimg_fea
+                cost[:, refimg_fea.size()[1]:, i, :,:]   = targetimg_fea
+        cost = cost.contiguous()
+
+        cost0 = self.dres0(cost)
+        cost0 = self.dres1(cost0) + cost0
+
+        out1, pre1, post1 = self.dres2(cost0, None, None) 
+        out1 = out1+cost0
+
+        out2, pre2, post2 = self.dres3(out1, pre1, post1) 
+        out2 = out2+cost0
+
+        out3, pre3, post3 = self.dres4(out2, pre1, post2) 
+        out3 = out3+cost0
+
+        cost1 = self.classif1(out1)
+        cost2 = self.classif2(out2) + cost1
+        cost3 = self.classif3(out3) + cost2
+
+        if self.training:
+            cost1 = F.interpolate(cost1, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+            cost2 = F.interpolate(cost2, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+
+            cost1 = torch.squeeze(cost1,1)
+            pred1 = F.softmax(cost1,dim=1)
+            pred1 = self.disparityregression(pred1)
+
+            cost2 = torch.squeeze(cost2,1)
+            pred2 = F.softmax(cost2,dim=1)
+            pred2 = self.disparityregression(pred2)
+
+        cost3 = F.interpolate(cost3, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+        cost3 = torch.squeeze(cost3,1)
+        pred3 = F.softmax(cost3,dim=1)
+        #For your information: This formulation 'softmax(c)' learned "similarity" 
+        #while 'softmax(-c)' learned 'matching cost' as mentioned in the paper.
+        #However, 'c' or '-c' do not affect the performance because feature-based cost volume provided flexibility.
+        pred3 = self.disparityregression(pred3)
+
+        if self.downsample < 1.:
+            if self.training:
+                pred1 = torch.squeeze(F.interpolate(torch.unsqueeze(pred1, 1), size=[left_origin.size()[2],left_origin.size()[3]]), 1)
+                pred2 = torch.squeeze(F.interpolate(torch.unsqueeze(pred2, 1), size=[left_origin.size()[2],left_origin.size()[3]]), 1)
+                pred3 = torch.squeeze(F.interpolate(torch.unsqueeze(pred3, 1), size=[left_origin.size()[2],left_origin.size()[3]]), 1)
+            else:
+                pred3 = torch.squeeze(F.interpolate(torch.unsqueeze(pred3, 1), size=[left_origin.size()[2],left_origin.size()[3]]), 1)
+
+
         if self.training:
             return pred1, pred2, pred3
         else:
-            return None
+            return pred3 
+            return None 
+            
